0db1be78ba1d0dbfb4e10e7bcd45e663c047efed
Rishi Lekkala <lekkalarishi9@gmail.com>
2025-12-14 18:07:49 +0530

    Add health endpoint

--- src/test/java/com/pl/premier_zone/PremierZoneApplicationTests.java
+++ src/test/java/com/pl/premier_zone/PremierZoneApplicationTests.java
@@ -3,7 +3,10 @@ package com.pl.premier_zone;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+
 @SpringBootTest
+@ActiveProfiles("test")
 class PremierZoneApplicationTests {
 
 	@Test
